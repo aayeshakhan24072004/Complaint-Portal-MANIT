2ce3b62f4a976fd72be2623ebb639618805a71f0
aayeshakhan24072004 <157532799+aayeshakhan24072004@users.noreply.github.com>
2025-06-26 15:21:52 +0530

    Update db.js

--- Complaint-Portal-MANIT-Backend-main/config/db.js
+++ Complaint-Portal-MANIT-Backend-main/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI);  // ✅ corrected here
+        const conn = await mongoose.connect(process.env.MONGODB_URI);  //corrected here
         console.log(`MongoDB Connected Successfully`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
